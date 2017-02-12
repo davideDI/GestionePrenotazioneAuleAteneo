11d112bb30e9559879f04b5551559eed404dfb8c
davideDI <davide.de.innocentis@gmail.com>
2017-02-12 22:16:26 +0100

    aggiunta query esempio + integrazione calendario

--- app/Booking.php
+++ app/Booking.php
@@ -4,7 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Booking extends Model
-{
-    //
+class Booking extends Model {
+    
+    
+    
 }
--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-
+use Illuminate\Support\Facades\DB;
 
 class BookingController extends Controller {
 
-    //switch language
     public function getGroupById($idGroup) {
         
-        return view('pages/index-calendar', [ 'group' => \App\Group::find($idGroup)]);
+        $bookings = DB::table('bookings')
+            ->select('bookings.name as book_name', 'events.event_date_start as start_date', 'events.event_date_end as end_date')
+            ->leftJoin('resources', 'bookings.id_resource', '=', 'resources.id')
+            ->leftJoin('groups', 'resources.id_group', '=', 'groups.id')
+            ->leftJoin('events', 'bookings.id_event', '=', 'events.id')
+            ->where('groups.id', '=', $idGroup)
+            ->get();
+        
+        return view('pages/index-calendar', [ 'bookings' => $bookings]);
         
     }
 
