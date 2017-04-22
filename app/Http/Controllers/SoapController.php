3de7eb427471f2537b459c259b4e9987e80ff410
davideDI <davide.de.innocentis@gmail.com>
2017-04-22 15:17:06 +0200

    fake users

--- app/Http/Controllers/SoapController.php
+++ app/Http/Controllers/SoapController.php
@@ -14,70 +14,125 @@ class SoapController extends Controller {
         $this->soapWrapper = $soapWrapper;
     }
   
+    private function checkFakeUsersForLogin($username) {
+        
+        if($username == 'davide@davide.it') {
+            session(['session_id' => '999ooo888iii']);
+            session(['source_id' => '654321']);
+            session(['nome'      => 'DAVIDE']);
+            session(['cognome'   => 'DAVIDE']);
+            session(['cod_fis'   => 'DAVIDEDAVIDE33']);
+            session(['ruolo'     => 'admin']);
+            session(['matricola' => 'davide@davide']);
+            return true;
+        }
+        
+        else if($username == 'ateneo@ateneo.it') {
+            session(['session_id' => '222eee333rrr']);
+            session(['source_id' => '123456']);
+            session(['nome'      => 'ATENEO']);
+            session(['cognome'   => 'ATENEO']);
+            session(['cod_fis'   => 'ATENEOATENEO33']);
+            session(['ruolo'     => 'admin']);
+            session(['matricola' => 'ateneo@ateneo.it']);
+            return true;
+        }
+        
+        else {
+            return false;
+        }
+    }
+    
     public function wsLogin() {
         
-        $this->soapWrapper->add('GenericWSEsse3', function ($service) {
-            $service->wsdl('https://segreteriavirtuale.univaq.it/services/ESSE3WS?wsdl');
-        });
-
-        $fn_dologin = $this->soapWrapper->call('GenericWSEsse3.fn_dologin', [
-            'username' => $_POST['username'],
-            'password' => $_POST['password']
-        ]);
+        $username = $_POST['username'];
+        $password = $_POST['password'];
         
-        $sessionID = (string) $fn_dologin['sid'];
-        $sid = 'SESSIONID='.$sessionID;
-        
-        $fn_retrieve_xml_p = $this->soapWrapper->call('GenericWSEsse3.fn_retrieve_xml_p', [
-            'retrieve' => 'GET_ANAG_FROM_SESSION',
-            'params'   => $sid
-        ]);
-        
-        //Codice di risposta
-        $responseCode = $fn_retrieve_xml_p['fn_retrieve_xml_pReturn'];
-        
-        //se il codice di risposta è 1 non ci sono stati errori
-        if($responseCode == 1) {
-        
-            //Xml di risposta
-            $response = new \SimpleXMLElement($fn_retrieve_xml_p['xml']);
-
-            $row = $response->children()->children()->children();
-            $sourceID = (string) $row->SOURCE_ID;
-            $nome = (string) $row->NOME;
-            $cognome = (string) $row->COGNOME;
-            $codFis = (string) $row->COD_FIS;
-            $ruolo = (string) $row->RUOLO;
-            $matricola = (string) $row->MATRICOLA;
-
-            session(['session_id' => $sessionID]);
-            session(['source_id' => $sourceID]);
-            session(['nome'      => $nome]);
-            session(['cognome'   => $cognome]);
-            session(['cod_fis'   => $codFis]);
-            session(['ruolo'     => $ruolo]);
-            session(['matricola' => $matricola]);
-        
+        //gestione utenza fittizia per sviluppo
+        if($this->checkFakeUsersForLogin($username)) {
             return redirect('/');
-            
         } else {
-            return Redirect::back()->withErrors([$responseCode]);
+        
+            $this->soapWrapper->add('GenericWSEsse3', function ($service) {
+                $service->wsdl('https://segreteriavirtuale.univaq.it/services/ESSE3WS?wsdl');
+            });
+
+            $fn_dologin = $this->soapWrapper->call('GenericWSEsse3.fn_dologin', [
+                'username' => $username,
+                'password' => $password
+            ]);
+
+            $sessionID = (string) $fn_dologin['sid'];
+            $sid = 'SESSIONID='.$sessionID;
+
+            $fn_retrieve_xml_p = $this->soapWrapper->call('GenericWSEsse3.fn_retrieve_xml_p', [
+                'retrieve' => 'GET_ANAG_FROM_SESSION',
+                'params'   => $sid
+            ]);
+
+            //Codice di risposta
+            $responseCode = $fn_retrieve_xml_p['fn_retrieve_xml_pReturn'];
+
+            //se il codice di risposta è 1 non ci sono stati errori
+            if($responseCode == 1) {
+
+                //Xml di risposta
+                $response = new \SimpleXMLElement($fn_retrieve_xml_p['xml']);
+
+                $row = $response->children()->children()->children();
+                $sourceID = (string) $row->SOURCE_ID;
+                $nome = (string) $row->NOME;
+                $cognome = (string) $row->COGNOME;
+                $codFis = (string) $row->COD_FIS;
+                $ruolo = (string) $row->RUOLO;
+                $matricola = (string) $row->MATRICOLA;
+
+                session(['session_id' => $sessionID]);
+                session(['source_id' => $sourceID]);
+                session(['nome'      => $nome]);
+                session(['cognome'   => $cognome]);
+                session(['cod_fis'   => $codFis]);
+                session(['ruolo'     => $ruolo]);
+                session(['matricola' => $matricola]);
+
+                return redirect('/');
+
+            } else {
+                return Redirect::back()->withErrors([$responseCode]);
+            }
+           
         }
 
     }
     
+    private function checkFakeUsersForLogout() {
+        
+        $matricola = session('matricola');
+        
+        if($matricola == 'davide@davide.it' || $matricola == 'ateneo@ateneo.it') {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
     public function wsLogout() {
         
-        $sessionId = session('session_id');
-        $sid = 'SESSIONID='.$sessionId;
+        if(!$this->checkFakeUsersForLogout()) {
+            
+            $sessionId = session('session_id');
+            $sid = 'SESSIONID='.$sessionId;
+
+            $this->soapWrapper->add('GenericWSEsse3', function ($service) {
+                $service->wsdl('https://segreteriavirtuale.univaq.it/services/ESSE3WS?wsdl');
+            });
+
+            $fn_doLogout = $this->soapWrapper->call('GenericWSEsse3.fn_doLogout', [
+                'params' => $sid
+            ]);
         
-        $this->soapWrapper->add('GenericWSEsse3', function ($service) {
-            $service->wsdl('https://segreteriavirtuale.univaq.it/services/ESSE3WS?wsdl');
-        });
-        
-        $fn_doLogout = $this->soapWrapper->call('GenericWSEsse3.fn_doLogout', [
-            'params' => $sid
-        ]);
+        }
         
         Session::forget('session_id');
         Session::forget('source_id');
