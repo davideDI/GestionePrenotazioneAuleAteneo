f151fa8e662513aee18485a59227db02a70d5ced
davideDI <davide.de.innocentis@gmail.com>
2017-07-26 17:22:04 +0200

    download pdf

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 use Spipu\Html2Pdf\Html2Pdf;
 use Illuminate\Http\Request;
 
@@ -19,12 +20,11 @@ class PrintController extends Controller {
     
     }
     
+    //TODO da concordare contenuti delle stampe
     public function downloadPDF(Request $request) {
 
         Log::info('PrintController - downloadPDF()');
-        
-        //TODO da concordare contenuti delle stampe
-        
+
         $idResource = $request['resource_id'];
         
         $dateSearchFromString = $request['date_search_from'];
@@ -43,20 +43,62 @@ class PrintController extends Controller {
             $dateSearchTo = "9999-01-01";
         }
         
-        //TODO completare query con filtro date
-        $bookingList = \App\Booking::with('repeats')
-                                    ->where('resource_id', $idResource)
-//                                    ->where('repeat.event_date_start', '>=', $dateSearchFrom)
-//                                    ->where('repeat.event_date_end', '<=', $dateSearchTo)
-                                    ->get();
+        //TODO modifica query per utilizzare eloquent (le query annidate si riscrivono)
+//        $bookingList = \App\Booking::with(['repeats' => function($query) use($dateSearchFrom, $idResource, $dateSearchTo) {
+//                                                Log::info($dateSearchFrom.'-'.$dateSearchTo);
+//                                                $query->where('event_date_start', '>=', $dateSearchFrom)
+//                                                      ->where('event_date_end', '<=', $dateSearchTo)
+//                                                        ->where('resource_id', $idResource);
+//                                            }])
+//                                    
+//                                    ->get();
+        
+        $listOfParameters = array($idResource, 3, $dateSearchFrom, $dateSearchTo);
+        $bookingList = DB::select
+                        ( DB::raw
+                            ("  select bookings.name, bookings.description,  
+                                resources.name as resource_name,
+                                repeats.event_date_start, repeats.event_date_end
+                                from bookings 
+                                left join repeats 
+                                on bookings.id = repeats.booking_id 
+                                left join resources
+                                on bookings.resource_id = resources.id 
+                                where resource_id = ? 
+                                and tip_booking_status_id = ?
+                                and repeats.event_date_start >= ? 
+                                and repeats.event_date_end <= ?
+                            ")
+                            , 
+                            $listOfParameters
+                        );
+ Log::info($bookingList);
 
-        $content = "";
+        $content = "<table>";
+        $content .=  "<thead>";
+        $content .=  "<tr>";
+        $content .=  "<th>Title</th>";
+        $content .=  "<th>Description</th>";
+        $content .=  "<th>Date from</th>";
+        $content .=  "<th>Date end</th>";
+        $content .=  "<th>Resource</th>";
+        $content .=  "</tr>";
+        $content .=  "</thead>";
+        $content .=  "<tbody>";
         //TODO compleatare tabella con informazioni
         if(count($bookingList) > 0) {
             for($i = 0; $i < count($bookingList); $i++) {
-                $content .= $bookingList[$i]->name;
+                $content .=  "<tr>";
+                $content .=  "<td>".$bookingList[$i]->name."</td>";
+                $content .=  "<td>".$bookingList[$i]->description."</td>";
+                $content .=  "<td>".$bookingList[$i]->event_date_start."</td>";
+                $content .=  "<td>".$bookingList[$i]->event_date_end."</td>";
+                $content .=  "<td>".$bookingList[$i]->resource_name."</td>";
+                $content .=  "</tr>";
             }
         }
+        $content .=  "</tbody>";
+        $content .=  "</table>";
         
         $html2pdf = new Html2Pdf('P','A4','it');
         $html2pdf->writeHTML($content);
